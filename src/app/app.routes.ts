d77ad8092c5eb573ab27ad80c6904e3773197cd6
jmbsfc <a2225a13@estel.edu.pt>
2024-11-26 20:36:28 +0000

    1st commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PerfilComponent } from './components/perfil/perfil.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'perfil', component: PerfilComponent}
+];
