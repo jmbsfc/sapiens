98c22fcff9ea73f8d5122e53c8bbceea4f2877f0
jmbsfc <a2225a13@estel.edu.pt>
2024-12-11 23:08:58 +0000

    "Organizations" development
    -added cards and ts behind it
    -edited readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Sapiens
 
+Sapiens is an App that helps connect volunteers to organizations.
+
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.0.2.
 
 ## Development server
--- src/app/components/oportunidades/oportunidades.component.ts
+++ src/app/components/oportunidades/oportunidades.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardComponent } from '../card/card.component';
+import { OportunidadesService } from '../../services/oportunidades.service';
+import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-oportunidades',
-  imports: [],
+  imports: [CardComponent,NgFor],
   templateUrl: './oportunidades.component.html',
   styleUrl: './oportunidades.component.css'
 })
-export class OportunidadesComponent {
+export class OportunidadesComponent implements OnInit {
+  // vou criar uma variável para os dados
+  data: any;
 
+  constructor(private oportunidadesService: OportunidadesService) {}
+  
+  ngOnInit(){
+    // aqui vou aribuir os dados do serviçao aka JSON à variável
+    this.oportunidadesService.getJSON().subscribe(data => {
+      this.data = data;
+      this.data.map((oneData:any) => 
+        console.log(oneData)
+
+      )
+    })
+  }
 }
