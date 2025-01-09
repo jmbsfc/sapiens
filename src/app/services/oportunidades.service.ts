7d6b42efdaad46bfb96e1ee5909d24c057996457
? <rm131700@gmail.com>
2025-01-09 18:09:01 +0000

    chamar api
    zzz

--- src/app/services/oportunidades.service.ts
+++ src/app/services/oportunidades.service.ts
@@ -14,6 +14,17 @@ export class OportunidadesService {
   }
 
   public getJSON(): Observable<any> {
-      return this.http.get("./assets/offers.json")
+      return this.http.get("./assets/offers.json")  
+
+     // this.http.post("http:/localhost:8080/volunteers", {
+     //     "firstName": "Ricardo",
+     //     "lastName": "Moreira",
+     //     "imageUrl": "/images/4feae695-1ec9-4566-8631-da1fd81d2d09.png",
+     //    "email": "rm131700@gmail.com",
+     //     "password": "Ricardo10",
+     //     "birthday": "29-10-2006",
+     //     "phoneNumber": "932569990",
+     //     "civilId": "031677247"
+     // })
   }
 }
