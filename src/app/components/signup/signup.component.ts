35f9a32657771138193b64c3f92c091db7f4d131
? <a2225a13@estel.edu.pt>
2025-02-13 18:11:03 +0000

    Multistep
    need to find out how this works

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrls: ['./signup.component.css'],
+  standalone: true,
+  imports: [FormsModule] // Importa FormsModule diretamente no componente
 })
 export class SignupComponent {
+  step: number = 1;
+  formData: any = {
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+    profilePicture: null,
+    phone: '',
+    civilId: '',
+    birthdate: ''
+  };
 
+  nextStep() {
+    if (this.step < 2) {
+      this.step++;
+    }
+  }
+
+  prevStep() {
+    if (this.step > 1) {
+      this.step--;
+    }
+  }
+
+  onFileChange(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.formData.profilePicture = file;
+    }
+  }
+
+  onSubmit() {
+    console.log('Form Data:', this.formData);
+    // Adicionar logica
+  }
 }
